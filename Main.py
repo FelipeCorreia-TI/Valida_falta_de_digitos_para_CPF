a62913a9db039e0203d38a54b8e7a89c0c02ff9d
Felipe Correia <felipecorreiatech@gmail.com>
2026-05-12 13:13:38 -0300

    Create main.py

---
+++ Main.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+
+def abre_arquivo(): #Retorna o arquivo aberto para analise e edição
+    Tk().withdraw()
+
+    caminho_arquivo = askopenfilename(
+        title="Selecione um arquivo!",
+        filetypes=[
+        ("Texto", "*.txt")
+        ]
+    )
+    with open(caminho_arquivo,"r",encoding="latin-1") as f: #Abre o arquivo e captura o conteúdo
+        abertura_arquivo= f.read()
+    
+    
+    return abertura_arquivo
+
+informacao_arquivo = abre_arquivo() #Abre o arquivo
+
+
+quebra_linha = informacao_arquivo.splitlines() #Divide o arquivo em linhas cada linha dentro de uma lista de str
+
+erros = [
+    i for i in quebra_linha
+    if not (i[0:10].isdigit() and i[11:13] == "  ")
+] #Valida se o campo que deveria ter cpf está com 11 digitos e se existe o dblspace entre o campo cpf e nome;
+#Logo após as linhas que não cumprirem a condição serão armazenadas dentro da lista 'erros'
+
+
+#Caso a lista 'erros' seja verdadeira, ou seja, contenha algum elemento, um for percorre e printa suas listas
+if erros: 
+    for e in erros:
+        print(e)
+else:
+    print('Documento não contém erros de CPF e formatação') #Caso não tenha erros será exibida essa mensagem.
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
